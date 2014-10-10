a414690157864a3e89f164f069c7ca397845ff71
Trey Caliva <tcaliva@directorschoice.travel>
2014-10-10 11:24:11 -0500

    Added Airline API

--- lib/flightstats/airline.rb
+++ lib/flightstats/airline.rb
@@ -6,5 +6,25 @@ module FlightStats
                   :name,
                   :phone_number,
                   :active
+
+    @@base_path = "/flex/airlines/rest/v1/json"
+
+    class << self
+      def actives(options = {})
+        from_response API.get("#{base_path}/active", {}, options), 'airlines'
+      end
+
+      def actives_for_date(year, month, day, options = {})
+        from_response API.get("#{base_path}/active/#{year}/#{month}/#{day}", {}, options), 'airlines'
+      end
+
+      def all(options = {})
+        from_response API.get("#{base_path}/all", {}, options), 'airlines'
+      end
+
+      def base_path
+        @@base_path
+      end	
+    end
   end
 end
